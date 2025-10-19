0c2ec8cd59a3e4dd41f82cd1d28473d209daf2c1
Aleksandr Peretyatko <aap120281@gmail.com>
2025-10-19 20:31:52 +0300

    changed methods:clear() and save()

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,26 +5,37 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null) {
+                storage[i] = null;
+            }
+        }
     }
 
-    void save(Resume r) {
-    }
+        void save (Resume r){
+            for (int i = 0; i < storage.length; i++) {
+                if (storage[i] == null) {
+                    storage[i] = r;
+                }
+            }
+        }
 
-    Resume get(String uuid) {
-        return null;
-    }
+        Resume get (String uuid){
+            return null;
+        }
 
-    void delete(String uuid) {
-    }
+        void delete (String uuid){
+        }
 
-    /**
-     * @return array, contains only Resumes in storage (without null)
-     */
-    Resume[] getAll() {
-        return new Resume[0];
-    }
+        /**
+         * @return array, contains only Resumes in storage (without null)
+         */
+        Resume[] getAll () {
+            return new Resume[0];
+        }
 
-    int size() {
-        return 0;
-    }
+        int size () {
+            return 0;
+        }
+
 }
