4fa7899227198810096aa574e7963185247f44a6
Aleksandr Peretyatko <aap120281@gmail.com>
2025-10-21 20:09:08 +0300

    changed methods:delete(),get(),getAll(),size()

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -12,30 +12,69 @@ public class ArrayStorage {
         }
     }
 
-        void save (Resume r){
-            for (int i = 0; i < storage.length; i++) {
-                if (storage[i] == null) {
-                    storage[i] = r;
-                }
+    void save(Resume r) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
+                storage[i] = r;
+                break;
             }
         }
+    }
 
-        Resume get (String uuid){
-            return null;
+    Resume get(String uuid) {
+        Resume result= new Resume();
+        for (Resume resume : storage) {
+            if (resume.uuid.equals(uuid)) {
+                result = resume;
+                break;
+            }
+        }
+        return result;
+    }
+
+    void delete(String uuid) {
+
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null && storage[i].uuid.equals(uuid)) {
+                storage[i] = null;
+            }
+        }
+        Resume [] resumeDeleted = new Resume[storage.length-1];
+        int index = 0;
+        for (Resume resume : storage) {
+            if (resume != null) {
+                resumeDeleted[index++] = resume;
+            }
+        }
+        storage = resumeDeleted;
+    }
+
+    /**
+     * @return array, contains only Resumes in storage (without null)
+     */
+    Resume[] getAll() {
+        Resume[] newStorage = new Resume[storage.length]; // Максимальный размер
+
+        int index = 0;
+        for (Resume resume : storage) {
+            if (resume != null) {
+                newStorage[index++] = resume;
+            }
+
+        }return newStorage;
+    }
+
+        int size() {
+            int count = 0;
+            for (Resume resume : storage) {
+                if (resume != null) {
+                    count++;
+                }
+            }
+
+            return count;
         }
 
-        void delete (String uuid){
-        }
 
-        /**
-         * @return array, contains only Resumes in storage (without null)
-         */
-        Resume[] getAll () {
-            return new Resume[0];
-        }
-
-        int size () {
-            return 0;
-        }
 
 }
